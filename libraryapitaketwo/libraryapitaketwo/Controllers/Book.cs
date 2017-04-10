1f525dce3dbe4cab8d705ec401dd7b783b7d60b1
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-10 15:48:24 -0400

    get is working

--- libraryapitaketwo/libraryapitaketwo/Controllers/Book.cs
+++ libraryapitaketwo/libraryapitaketwo/Controllers/Book.cs
@@ -1,19 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
-namespace libraryapitaketwo.Controllers
+namespace libraryapitaketwo.Models
+
 {
     public class Book
     {
+        public Book() { }
+        public Book(SqlDataReader reader)
+        {
+            this.Id = (int)reader["id"];
+            this.Title = reader["Title"].ToString();
+            this.Author = reader["Author"].ToString();
+            this.YearPublished = reader["YearPublished"] as int?;
+            this.Genre = reader["Genre"].ToString();
+            this.IsCheckedOut = reader["IsCheckedOut"] as bool?;
+            this.LastCheckedOutDate = reader["LastCheckedOutDate"] as DateTime?;
+            this.DueBackDate = reader["DueBackDate"] as DateTime?;
+        }
+
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
-        public int YearPublished { get; set; }
+        public int? YearPublished { get; set; }
         public string Genre { get; set; }
-        public bool IsCheckedOut { get; set; }
-        public DateTime LastCheckedOutDate { get; set; }
-        public DateTime DueBackDate { get; set; }
+        public bool? IsCheckedOut { get; set; }
+        public DateTime? LastCheckedOutDate { get; set; }
+        public DateTime? DueBackDate { get; set; }
     }
 }
--- libraryapitaketwo/libraryapitaketwo/Controllers/LibraryController.cs
+++ libraryapitaketwo/libraryapitaketwo/Controllers/LibraryController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
+using libraryapitaketwo.Models;
+using System.Data.SqlClient;
 
 namespace libraryapitaketwo.Controllers
 {
     public class LibraryController : ApiController
     {
-        public void GetAllBooks()
+        [HttpGet]
+        public List<Book> GetAllBooks()
         {
+            const string connectionString = 
+                            @"Server=localhost\SQLEXPRESS;Database=libraryapi;Trusted_Connection=True;";
 
+            var rv = new List<Book>();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query = "SELECT * FROM LibraryTable";
+                var cmd = new SqlCommand(query, connection);
+                connection.Open();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    rv.Add(new Book(reader));
+                }
+                connection.Close();
+            }
+            return rv;
         }
+
     }
 }
